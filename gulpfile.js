9446dc22391b5e0be0fef73f9d0d2ae50402d078
stairwaytoeve <eve.ionica@gmail.com>
2017-08-01 23:36:00 -0300

    Add: comments

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+
+var srcPath = './dev/public/*';
+var buildPath = '.';
+
+
+gulp.task('copy', function() {
+  gulp.src(srcPath)
+    .pipe(gulp.dest(buildPath));
+});
+
+
+gulp.task('default', ['copy']);
